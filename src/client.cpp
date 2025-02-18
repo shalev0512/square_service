2b9ea93f199f12899b31301049f93e0e4321be10
shalev0512 <shalevmagshimim@gmail.com>
2025-02-18 16:37:01 +0200

    fixing squared num and adding my own features

--- src/client.cpp
+++ src/client.cpp
@@ -6,26 +6,31 @@ SquareIntClient::SquareIntClient(): Node(CLIENT_NAME)
     client_ = create_client<square_service::srv::SquareInt>("square_int");
 }
 
-void SquareIntClient::send_request(int64_t num){
-
+void SquareIntClient::send_request(int64_t num)
+{
     //waiting for service
-    while(!client_->wait_for_service(std::chrono::seconds(1))){
-        if(!rclcpp::ok()){
+    while(!client_->wait_for_service(std::chrono::seconds(1)))
+    {
+        if(!rclcpp::ok())
+        {
             RCLCPP_ERROR(this->get_logger(), "Interrupted while waiting for service.");
             return;
         }
         RCLCPP_INFO(this->get_logger(), "Waiting for service...");
     }
-
+    
     auto request = std::make_shared<square_service::srv::SquareInt::Request>();
     request->number = num;
-
+    
     // Use callback version for async request
-    client_->async_send_request(
+    client_->async_send_request
+    (
         request,
-        [this, num](rclcpp::Client<square_service::srv::SquareInt>::SharedFuture future) {
+        [this, num](rclcpp::Client<square_service::srv::SquareInt>::SharedFuture future) 
+        {
+            std::string isPrefectSquareRoot = future.get()->is_perfect_square_root ? "true": "false";
             RCLCPP_INFO(this->get_logger(), "Number: %ld", num);
-            RCLCPP_INFO(this->get_logger(), "Square: %lf Cube: %ld", future.get()->square, future.get()->cube);
+            RCLCPP_INFO(this->get_logger(), "Send Response - Squred Number: %ld & Cubed Number: %ld & Square Root Number: %lf & is perfect squareRoot: %s", future.get()->square, future.get()->cube, future.get()->square_root, isPrefectSquareRoot.c_str());
         }
     );
 }
--- src/service.cpp
+++ src/service.cpp
@@ -7,18 +7,30 @@
 
 SquareIntService::SquareIntService():Node(SERVICE_NAME)
 {
-    service_ = create_service<square_service::srv::SquareInt>(
+    service_ = create_service<square_service::srv::SquareInt>
+    (
         "square_int",
-        std::bind(&SquareIntService::handle_service, this, 
-            std::placeholders::_1, std::placeholders::_2));
+        std::bind(&SquareIntService::handle_service, this, std::placeholders::_1, std::placeholders::_2)
+    );
 }
 
 
 void SquareIntService::handle_service(const std::shared_ptr<square_service::srv::SquareInt::Request> request, std::shared_ptr<square_service::srv::SquareInt::Response> response)
 {
+    //vars
+    float reminder = 0;
     int64_t input_num = request->number;
-    response->square = pow(input_num, 0.5);
-    response->cube = pow(input_num, 2);
+    std::string isPrefectSquareRoot = "";
+
+    response->square = static_cast<int64_t>(pow(input_num, 2));
+    response->cube = static_cast<int64_t>(pow(input_num, 3));
+    response->square_root = static_cast<double>(pow(input_num, 0.5));
+
+    reminder = std::fmod(response->square_root, 1.0);
+    response->is_perfect_square_root = (reminder == 0.0);
+
+    isPrefectSquareRoot = response->is_perfect_square_root ? "true": "false";
+
     RCLCPP_INFO(this->get_logger(), "Got request - Number %ld", request->number);
-    RCLCPP_INFO(this->get_logger(), "Send Response - Squred Number: %lf & Cubed Number: %ld", response->square, response->cube);
+    RCLCPP_INFO(this->get_logger(), "Send Response - Squred Number: %ld & Cubed Number: %ld & Square Root Number: %lf & is perfect squareRoot: %s", response->square, response->cube, response->square_root, isPrefectSquareRoot.c_str());
 }
